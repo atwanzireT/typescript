b429230b8c33fcfd48d3e2c09ceebfece444ceaa
= <=>
2022-11-22 22:04:58 +0300

    function in typescript

--- basics.tsx
+++ basics.tsx
@@ -2,4 +2,20 @@
 const message = "Hello World" // each abstract value has a different form of behaviour
 
 // The above const can be covered to uppercase
-message.toUpperCase()
+console.log(message.toUpperCase())
+
+const num = 43
+// Assume a variable not holding a string is supplied with .toLowerCase or .toUpperCase
+// There will be a raise of an error
+// console.log(num.toLowerCase())
+
+
+// function typescripts
+function fn(x) {
+    return x.flip();
+   }
+   
+// console.log(fn("Heelo"))
+// console.log(fn(123))
+
+// the above function hits an error
